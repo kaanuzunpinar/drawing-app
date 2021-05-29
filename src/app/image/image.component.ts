813db40c423d2218a3b17cd23ccf0b4e16f74aa2
kaanuzunpinar <uzunpinarkaan@gmail.com>
2021-05-29 18:02:54 +0300

    image_upload

---
+++ src/app/image/image.component.ts
@@ -0,0 +1,24 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-image',
+  templateUrl: './image.component.html',
+  styleUrls: ['./image.component.css']
+})
+export class ImageComponent implements OnInit {
+
+  constructor() { }
+  url:string="";
+  ngOnInit(): void {
+  }
+  onFileChanged(event: any){
+    if(event.target.files && event.target.files[0]){
+      const reader = new FileReader();
+      reader.readAsDataURL(event.target.files[0]);
+      reader.addEventListener("load", (ev:any)=> {
+        this.url=ev.target.result;
+      }, false);
+    }
+    console.log(this.url);
+  }
+}
